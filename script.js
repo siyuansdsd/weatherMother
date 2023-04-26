154f825f5c7742905897af43b60eeeaddb3d6035
Douglas.Y <siyuan.yang2@uq.net.au>
2023-04-26 13:49:32 +1000

    not the final to css

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const api_key = '401d06340b9742dcb0d125152232304';
 var city_name = 'London';
 const kv = {};
 
+async function getValue() {
+    city_name = await document.getElementById("city").value;
+    search_weather_by_name_async(city_name);
+    console.log(city_name);
+}
+
 async function search_weather_by_name_async(city_name_alt) {
     city_name = city_name_alt;
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city_name}&aqi=no`);
@@ -11,10 +17,6 @@ async function search_weather_by_name_async(city_name_alt) {
     kv['country'] = data.location.country;
     kv['temp'] = data.current.temp_c;
     kv['icon'] = data.current.condition.icon;
-}
-
-async function search(city_name_alt) {
-    await search_weather_by_name_async(city_name_alt);
     document.getElementById('city').innerHTML = kv['city'];
     document.getElementById('country').innerHTML = kv['country'];
     document.getElementById('temp').innerHTML = kv['temp'];
