3c59825f9d16f0e44c2be535d97faa595bbf8d4a
Douglas.Y <siyuan.yang2@uq.net.au>
2023-04-30 00:30:53 +1000

    二次更新

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const api_key = '401d06340b9742dcb0d125152232304';
-var city_name = 'London';
-const kv = {};
+// const weather = document.getElementById("citty");
+// var city_name = "London";
+
+window.onload = aa();
+
+async function search_weather_by_name_async(city_name_alt) {
+    var city_name = city_name_alt;
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city_name}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            weather.innerHTML = "wocaonima";
+            document.getElementById('cityy').innerHTML = data.location.name;
+            console.log(document.getElementById('cityy').innerHTML)
+            document.getElementById('country').innerHTML = data.location.country;
+            document.getElementById('temp').innerHTML = data.current.temp_c;
+            document.getElementById('icon').src = data.current.condition.icon;
+        })
+}
 
 async function getValue() {
-    city_name = await document.getElementById("city").value;
-    search_weather_by_name_async(city_name);
+    var city_name = await document.getElementById("city").value;
+    await console.log(city_name);
+    await search_weather_by_name_async(city_name);
     console.log(city_name);
 }
 
-async function search_weather_by_name_async(city_name_alt) {
-    city_name = city_name_alt;
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city_name}&aqi=no`);
-    const data = await response.json();
-    console.log(data);
-    kv['city'] = data.location.name;
-    kv['country'] = data.location.country;
-    kv['temp'] = data.current.temp_c;
-    kv['icon'] = data.current.condition.icon;
-    document.getElementById('city').innerHTML = kv['city'];
-    document.getElementById('country').innerHTML = kv['country'];
-    document.getElementById('temp').innerHTML = kv['temp'];
-    document.getElementById('icon').src = kv['icon'];
+function aa() {
+    setTimeout(getValue(), 2000);
 }
