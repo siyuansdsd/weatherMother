63bcfca0f86797d1445afb04356929c41ca3c721
Douglas.Y <siyuan.yang2@uq.net.au>
2023-05-05 12:19:05 +1000

    初步实现功能

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
+// 获取即时天气信息
 const api_key = '401d06340b9742dcb0d125152232304';
-// const weather = document.getElementById("citty");
-// var city_name = "London";
-
-window.onload = aa();
 
 async function search_weather_by_name_async(city_name_alt) {
     var city_name = city_name_alt;
     fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city_name}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            weather.innerHTML = "wocaonima";
-            document.getElementById('cityy').innerHTML = data.location.name;
-            console.log(document.getElementById('cityy').innerHTML)
-            document.getElementById('country').innerHTML = data.location.country;
-            document.getElementById('temp').innerHTML = data.current.temp_c;
-            document.getElementById('icon').src = data.current.condition.icon;
+            document.getElementById('weather').innerHTML = `weather condition: ${data.current.condition.text}`;
+            document.getElementById('cityy').innerHTML = `Search place: ${data.location.name}`;
+            document.getElementById('country').innerHTML = `Country of search place: ${data.location.country}`;
+            document.getElementById('temp').innerHTML = `temperature in celsius: ${data.current.temp_c} ℃`;
+            document.getElementById('UV').innerHTML = `UV: ${data.current.uv}`;
+            document.getElementById('localTime').innerHTML = `Local time: ${data.location.localtime}`;
+            document.getElementById('updateTime').innerHTML = `Update time: ${data.current.last_updated}`;
+            // get the feel temperature from the api
+            document.getElementById('feelTemp').innerHTML = `Feel temperature: ${data.current.feelslike_c} ℃`;
         })
 }
 
 async function getValue() {
     var city_name = await document.getElementById("city").value;
-    await console.log(city_name);
-    await search_weather_by_name_async(city_name);
-    console.log(city_name);
+    search_weather_by_name_async(city_name);
 }
 
-function aa() {
-    setTimeout(getValue(), 2000);
-}
+document.getElementById("getBtn").addEventListener("click", (event) => {
+    event.preventDefault();
+    getValue();
+});
